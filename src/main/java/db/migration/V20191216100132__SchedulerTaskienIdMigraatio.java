3b86741d90ead2094271995d1ad7513c1996587d
Jani Kaarela <jani.kaarela@gmail.com>
2019-12-18 13:42:20 +0200

    Rekisteröintien migraatio bigserial id -> UUID.
    Migraatiot rekisteröintitaululle ja siihen viittaaville tauluille,
    Java-migraatio ajastettujen taskien migraatiolle (id taskien datassa
    serialisoituna).

--- src/main/java/db/migration/V20191216100132__SchedulerTaskienIdMigraatio.java
+++ src/main/java/db/migration/V20191216100132__SchedulerTaskienIdMigraatio.java
@@ -2,28 +2,30 @@ package db.migration;
 
 import org.flywaydb.core.api.migration.BaseJavaMigration;
 import org.flywaydb.core.api.migration.Context;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowCallbackHandler;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.datasource.SingleConnectionDataSource;
 import org.springframework.stereotype.Component;
 
 import java.io.*;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 @Component
 public class V20191216100132__SchedulerTaskienIdMigraatio extends BaseJavaMigration {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(
+            V20191216100132__SchedulerTaskienIdMigraatio.class);
     private static final String SELECT_REKISTEROINTI_ID_HISTORIA =
             "SELECT vanha_id, uusi_id FROM rekisterointi_id_historia";
     private static final String SELECT_SCHEDULED_TASKS =
-            "SELECT task_name, task_instance, task_data FROM scheduled_tasks WHERE task_name IN ('rekisterointi-email-task', " +
+            "SELECT task_name, task_data FROM scheduled_tasks WHERE task_name IN ('rekisterointi-email-task', " +
                     "'paatos-email-task', 'luo-tai-paivita-organisaatio-task', 'kutsu-kayttaja-task')";
-    private static final String UPDATE_SCHEDULED_TASKS =
-            "UPDATE scheduled_tasks SET task_data = ? WHERE task_name = ? AND task_instance = ?";
-
     @Override
     public void migrate(Context context) throws Exception {
         JdbcTemplate jdbcTemplate = new JdbcTemplate(
@@ -42,43 +44,9 @@ public class V20191216100132__SchedulerTaskienIdMigraatio extends BaseJavaMigrat
     }
 
     private void prosessoiTaskit(JdbcTemplate jdbcTemplate, Map<Long, UUID> idMap) {
-        final List<Object[]> argumentit = new ArrayList<>();
         jdbcTemplate.query(
                 SELECT_SCHEDULED_TASKS,
-                new ScheduledTaskRowMapper()).forEach(task -> {
-            Long vanhaId = deserialize(task.data);
-            UUID uusiId = idMap.get(vanhaId);
-            if (uusiId == null) {
-                throw new IllegalStateException("Rekisteröinnin uutta tunnistetta ei löydy, vanha tunniste: " + vanhaId);
-            }
-            argumentit.add(new Object[] { serialize(uusiId), task.name, task.instance });
-        });
-        jdbcTemplate.batchUpdate(
-                UPDATE_SCHEDULED_TASKS,
-                argumentit
-        );
-    }
-
-    private Long deserialize(byte[] data) {
-        ByteArrayInputStream byteStream = new ByteArrayInputStream(data);
-        try {
-            ObjectInput objectInput = new ObjectInputStream(byteStream);
-            return (Long) objectInput.readObject();
-        } catch (IOException | ClassNotFoundException e) {
-            throw new IllegalStateException("Rekisteröinnin tunnisteen lukeminen task datasta epäonnistui", e);
-        }
-    }
-
-    private byte[] serialize(UUID uuid) {
-        ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
-        try {
-            ObjectOutput objectOutput = new ObjectOutputStream(byteStream);
-            objectOutput.writeObject(uuid);
-            objectOutput.flush();
-            return byteStream.toByteArray();
-        } catch (IOException e) {
-            throw new IllegalStateException("Rekisteröinnin uuden tunnisteen muuntaminen tavuiksi epäonnistui", e);
-        }
+                new ScheduledTaskRowCallbackHandler(idMap));
     }
 
     private static class RekisterointiRowCallbackHandler implements RowCallbackHandler {
@@ -94,31 +62,53 @@ public class V20191216100132__SchedulerTaskienIdMigraatio extends BaseJavaMigrat
             Long vanhaId = resultSet.getLong("vanha_id");
             UUID uusiId = resultSet.getObject("uusi_id", UUID.class);
             idMap.put(vanhaId, uusiId);
+            LOGGER.debug("Luettu id-historiarivi: {} -> {}", vanhaId, uusiId);
         }
     }
 
-    private static class ScheduledTask {
-        private final String name;
-        private final String instance;
-        private final byte[] data;
-        private ScheduledTask(String taskName, String taskInstance, byte[] taskData) {
-            this.name = taskName;
-            this.instance = taskInstance;
-            this.data = taskData;
-        }
-    }
+    private static class ScheduledTaskRowCallbackHandler implements RowCallbackHandler {
 
-    private static class ScheduledTaskRowMapper implements RowMapper<ScheduledTask> {
+        private final Map<Long, UUID> idMap;
+
+        private ScheduledTaskRowCallbackHandler(Map<Long, UUID> idMap) {
+            this.idMap = idMap;
+        }
 
         @Override
-        public ScheduledTask mapRow(ResultSet resultSet, int i) throws SQLException {
+        public void processRow(ResultSet resultSet) throws SQLException {
             String taskName = resultSet.getString("task_name");
-            String taskInstance = resultSet.getString("task_instance");
             byte[] taskData = resultSet.getBytes("task_data");
-            return new ScheduledTask(taskName, taskInstance, taskData);
+            Long vanhaId = deserialize(taskData);
+            LOGGER.debug("Käsitellään task '{}' rekisteröinnille {}.", taskName, vanhaId);
+            UUID uusiId = idMap.get(vanhaId);
+            if (uusiId == null) {
+                throw new IllegalStateException("Rekisteröinnin uutta tunnistetta ei löydy, vanha tunniste: " + vanhaId);
+            }
+            resultSet.updateBytes("task_data", serialize(uusiId));
+            LOGGER.info("Asetettu taskin '{}' rekisteröintitunniste: {} -> {}", taskName, vanhaId, uusiId);
         }
 
+        private Long deserialize(byte[] data) {
+            ByteArrayInputStream byteStream = new ByteArrayInputStream(data);
+            try {
+                ObjectInput objectInput = new ObjectInputStream(byteStream);
+                return objectInput.readLong();
+            } catch (IOException e) {
+                throw new IllegalStateException("Rekisteröinnin tunnisteen lukeminen task datasta epäonnistui", e);
+            }
+        }
 
+        private byte[] serialize(UUID uuid) {
+            ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+            try {
+                ObjectOutput objectOutput = new ObjectOutputStream(byteStream);
+                objectOutput.writeObject(uuid);
+                objectOutput.flush();
+                return byteStream.toByteArray();
+            } catch (IOException e) {
+                throw new IllegalStateException("Rekisteröinnin uuden tunnisteen muuntaminen tavuiksi epäonnistui", e);
+            }
+        }
     }
 
 }
